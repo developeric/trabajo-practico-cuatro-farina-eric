4ccc007bc9268a6e458cde453c957dc3fb274a3c
develop <patinetakiller564@gmail.com>
2025-11-28 10:35:57 -0300

    se Creó el archivo fundamentales.ts

---
+++ src/tiposBasicos/fundamentales.ts
@@ -0,0 +1,6 @@
+const productName: string =
+  "Motomel 110 Color Negra con alarma y arranque electrico con llantas de aleación";
+console.log(productName);
+
+const productPrice: number = 2000000;
+console.log(productPrice);
